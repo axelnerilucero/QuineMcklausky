66cd85d143a4caa632aa861535069126feca44c0
axelnerilucero <neriluceroaxel@gmail.com>
2021-11-08 19:26:45 -0600

    la primera tabla de verdad

--- QuineMclausky.py
+++ QuineMclausky.py
@@ -12,3 +12,15 @@ def bintostr(numeros):
 
 letra = bintostr('--11-11-10')
 print(letra)
+
+def tablero(variables):
+    variables = int(input("Cuantas variables: ")) #aqui pa que le digas cuantas
+    tabla_vdd = [] #aqui va hacer la tablita
+    num_casas = ( (2) ** (variables)) # aqui van a ser los renglones que tiene la tabla
+    for cuadrito in range(0, num_casas): #lo recorremos para ir agregando los numeros
+        tabla_vdd.append(str(bin(cuadrito).replace("0b", ""))) #aqui vamos agregando los 0s y 1s, bin te da la representacion binaria de un numero pero con un 0b demas, por eso lo remplazamos por un espacio en blanco
+    return tabla_vdd
+
+tab = tablero(0)
+for x in tab:
+    print(x)
