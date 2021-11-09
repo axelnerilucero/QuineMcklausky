17b85967c588694db8b70ce1620933ebb67c8de7
Lizbeth <60724433+lizbeth-cortes@users.noreply.github.com>
2021-11-08 22:35:14 -0600

    Inserción de minitérminos

--- QuineMcClausky.py
+++ QuineMcClausky.py
@@ -1,49 +1,29 @@
+def tablero(variables):
+    variables = int(input("Cuantas variables: ")) #aqui pa que le digas cuantas
+    tabla_vdd = [] #aqui va hacer la tablita
+    num_casas = ( (2) ** (variables)) # aqui van a ser los renglones que tiene la tabla
+    for cuadrito in range(0, num_casas): #lo recorremos para ir agregando los numeros
+        tabla_vdd.append(str(bin(cuadrito).replace("0b", ""))) #aqui vamos agregando los 0s y 1s, bin te da la representacion binaria de un numero pero con un 0b demas, por eso lo remplazamos por un espacio en blanco
+    return tabla_vdd
 
-print("QuineMcClausky")
-print("Indroduza el número de variables")
-dato = int(input())
+tab = tablero(0)
 
-def verificar_variables(dato):
-    if not (dato>=2 and dato<=10):
-        print("Inserte entre 2 y 10 variables")
-        dato = int(input())
-        return dato
-verificar_variables(dato)
+for x in tab:
+    print(x)
 
-print("Introduzca el minitérmino seguido de una coma")
-numeros = input().split(",")
-
-def verificar_miniterminos(numeros):
-    numeros2=[int(x) for x in numeros]
-    for i in numeros2:
-        if not(i<(2**dato)):
-            print("Los minitérminos inician desde 0, introduzca nuevamente el minitérmino correcto")
-        else:
-            repetidos=numeros2.count(i)
-            if not (repetidos<2):
-                print("Se repite el minitérmino", i)
-
-verificar_miniterminos(numeros)
-
-columnas = int(dato)
-filas = 2**columnas
-matriz=[["0" for x in range(columnas)] for y in range(filas)]
-resultado=filas
-
-for r in matriz:
-    print(r)
+miniterminos = input("Introduza el número del minitérmino: ").split(",")
+numeros = [int(x) for x in miniterminos]
 
 
-def bintostr(numeros):
-    qtd = len(numeros)
-    palabra = []
-    for x in range(0, len(numeros)):
-        if numeros[x] == '1':
-            palabra.append(str(chr(65+x)))
-        if numeros[x] == '0':
-            palabra.append(str(chr(65 + x))+"'")
-    palabra_completa = ''.join([str(item) for item in palabra])
-    return palabra_completa
+def funcion(numeros):
+    funcion=[]
+    for i in tab:
+        for j in numeros:
+            if(tab.index(i)==j):
+                funcion.append(i)
+    return funcion
 
-letra = bintostr('--11-11-10')
-print(letra)
+fun=funcion(numeros)
+
+for z in fun:
+    print(z)
